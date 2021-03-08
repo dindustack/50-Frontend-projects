884adfa4a107c3827cacca24fc1dbb344d4bdcbd
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2021-03-08 23:40:53 +0100

    final fix

--- Drawing App/script.js
+++ Drawing App/script.js
@@ -1,11 +1,18 @@
 const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
 const ctx = canvas.getContext('2d');
 
-let size = 20
+let size = 10
 let isPressed = false
-let color = 'black'
+colorEl.value = 'black'
+let color = colorEl.value
 let x
-let y  
+let y
 
 canvas.addEventListener('mousedown', (e) => {
     isPressed = true
@@ -42,7 +49,7 @@ function drawCircle(x, y) {
 }
 
 function drawLine(x1, y1, x2, y2) {
-    ctx.beginPath();
+    ctx.beginPath()
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
     ctx.strokeStyle = color
@@ -50,4 +57,30 @@ function drawLine(x1, y1, x2, y2) {
     ctx.stroke()
 }
 
-drawCircle(100, 200)
+function updateSizeOnScreen() {
+    sizeEL.innerText = size
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5
+
+    if(size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+
+    if(size < 5) {
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height))
